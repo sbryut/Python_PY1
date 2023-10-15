e7ba0f82cbd34d5b79da1e0aac78a99ed3da1e84
sbryut <118308801+sbryut@users.noreply.github.com>
2023-10-15 23:17:12 +0300

    Delete Lab1 directory

--- Lab1/main1.py
+++
@@ -1,9 +0,0 @@
-numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-average = sum(filter(None, numbers)) / len(numbers)
-index = 0
-for element in numbers:
-    if element is None:
-        break
-    index += 1
-numbers[index] = average
-print("Измененный список:", numbers)
--- Lab1/main2.py
+++
@@ -1,9 +0,0 @@
-list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-
-# TODO Разделите участников на две команды
-
-len_ = len(list_players)
-first_team = list_players[:len_//2]
-second_team = list_players[len_//2:]
-print(first_team)
-print(second_team)
