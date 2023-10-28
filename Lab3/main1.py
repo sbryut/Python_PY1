df386a55c7ddc0789478d45eaf675dcc8ec814d8
sbryut <118308801+sbryut@users.noreply.github.com>
2023-10-28 10:35:43 +0300

    Add Lab3

---
+++ Lab3/main1.py
@@ -0,0 +1,14 @@
+def find_index_item(items, required_item):
+    for index, current_item in enumerate(items):
+        if current_item == required_item:
+            return index
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_index_item(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
