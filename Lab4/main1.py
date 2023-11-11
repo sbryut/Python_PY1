e3163a25a95be98dd5397b19240e8ed514084ad3
sbryut <118308801+sbryut@users.noreply.github.com>
2023-11-11 09:30:34 +0300

    adding solution

--- Lab4/main1.py
+++ Lab4/main1.py
@@ -1,6 +1,17 @@
-# TODO решите задачу
-def task() -> float:
-    ...
+import json
 
+
+def task() -> float:
+    with open("input.json", "r") as input_f:
+        data = json.load(input_f)
+
+    total = 0.0
+    for review in data:
+        score = review["score"]
+        weight = review["weight"]
+        product = score * weight
+        total += product
+
+    return round(total, 3)
 
 print(task())
--- Lab4/main2.py
+++ Lab4/main2.py
@@ -1,20 +1,24 @@
-# TODO импортировать необходимые молули
-
+import csv
+import json
 
 INPUT_FILENAME = "input.csv"
 OUTPUT_FILENAME = "output.json"
 
 
 def task() -> None:
-    ...  # TODO считать содержимое csv файла
+    with open(INPUT_FILENAME, "r") as input_f:
+        reader = csv.DictReader(input_f, delimiter=",")
 
-    ...  # TODO Сериализовать в файл с отступами равными 4
+        data = []
+        for row in reader:
+            data.append(row)
+
+    with open(OUTPUT_FILENAME, "w") as output_f:
+        output_f.write(json.dumps(data, indent=4))
 
 
-if __name__ == '__main__':
-    # Нужно для проверки
+if __name__ == "__main__":
     task()
-
     with open(OUTPUT_FILENAME) as output_f:
         for line in output_f:
             print(line, end="")
